740590c27fa64f5cc8943ba23d485c14a3bf69fb
Sean Ho <sean.ho@rea-group.com>
2018-07-14 15:58:50 +1000

    Add automation script to publish

---
+++ .auto/publish.sh
@@ -0,0 +1,28 @@
+#! /bin/bash -eu
+
+git config user.name $CD_GIT_AGENT_NAME
+git config user.email $CD_GIT_AGENT_EMAIL
+
+echo "Cloning repository"
+git clone --single-branch git@github.com:conceptable/conceptable.github.io.git tmp
+
+rm -rf tmp/*
+cp -r dist/* tmp
+
+cd tmp
+
+git add -A .
+if git diff --cached --exit-code; then
+  echo "No changes"
+  exit 0
+fi
+
+echo "Committing changes"
+git commit -m "[Auto] Update homepage"
+git push origin master
+
+echo "Cleaning up"
+cd ..
+rm -rf tmp
+
+echo "Published successfully"
--- content/hk_weather_description.md
+++ content/hk_weather_description.md
@@ -1,1 +1,3 @@
-Morbi in sem quis dui placerat ornare. Pellentesquenisi euismod in, pharetra a, ultricies in diam sed arcu. Cras consequat egestas augue vulputate.
+Morbi in sem quis dui placerat ornare. Pellentesquenisi
+euismod in, pharetra a, ultricies in diam sed arcu. Cras
+consequat egestas augue vulputate.
